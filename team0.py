30c1cf1277c8a4c62fb4dd459e0b27d7031d9e1b
MrsMa5 <41687969+MrsMa5@users.noreply.github.com>
2019-02-20 15:44:52 -0500

    Update team0.py

--- team0.py
+++ team0.py
@@ -8,33 +8,29 @@
 
 team_name = 'DNME-MAS' # Only 10 chars displayed.
 strategy_name = 'Every1 is the NME!'
-strategy_description = 'Playing The Percentages'
+strategy_description = 'Always c except when the other team b then I b except 1/7 of the time I can change my mind and c'
+import random
     
 def move(my_history, their_history, my_score, their_score):
-    opp_b = 0
-    opp_c = 0
+    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    # their_history: a string of the same length as history, possibly empty. 
+    # The first round between these two players is my_history[0] and their_history[0].
+    # The most recent round is my_history[-1] and their_history[-1].
     
-    if len(my_history) < 10:
-        return 'c'
-    elif len(my_history) < 20:
-        return 'b'
-    else:
-        for choice in their_history:
-            if choice == 'b':
-                opp_b += 1
-            else:
-                opp_c += 1
-                
-        if opp_c != 0:
-            percentage = float(opp_b)/opp_c
-        else:
-            return 'b'
-            
-        if percentage >= 1:
-            return 'b'
-        else:
-            return 'c'
-    
- 
+    # Analyze my_history and their_history and/or my_score and their_score.
+    # Decide whether to return 'c' or 'b'.
 
-    
+    '''    
+    Make my move.
+    Returns 'c' or 'b'. 
+    '''
+    if len(my_history)==0:
+        return 'c'
+    else:
+        if their_history[-1]=='b':
+            if random.randint(1,7)==3:
+                return 'c'
+            else:
+                return 'b'
+        else:
+            return 'c'    
