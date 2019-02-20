f3e73071b1fa0d4f81bd73c3f2bed55f9e66213a
MrsMa5 <41687969+MrsMa5@users.noreply.github.com>
2019-02-20 15:23:26 -0500

    Update team0.py

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'DNME-MAS' # Only 10 chars displayed.
+strategy_name = 'Every1 is the NME!'
+strategy_description = 'b first, collude 10 times, check last 5 moves, if b 1 time, then b, else c.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
