63f233bfe490c566fadc43ab4044d0c865be6e91
Ray Romero <46662592+Ray-Romero@users.noreply.github.com>
2019-02-22 11:28:45 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -17,15 +17,6 @@ def move(my_history, their_history, my_score, their_score):
         if their_score == 0:
             return 'c'
         else:
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
             if 'b' in their_history:
                 return 'b'
             else:
